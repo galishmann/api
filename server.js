c5cd9f0a42128318c779d2178db46de714e4aee3
Galishnikov <galishmann@sikoga.com>
2020-11-25 13:26:09 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const morgan = require('morgan');
+
+
+const data = [
+  {
+    id: '1',
+    title: 'One',
+  },
+  {
+    id: '2',
+    title: 'Two',
+  },
+  {
+    id: '3',
+    title: 'Three',
+  },
+];
+
+const app = express();
+app.use(morgan('tiny'));
+
+app.get('/api/healthcheck', (req, res) => {
+  res.send("ok");
+});
+
+app.get('/api/items', (req, res) => {
+  res.json(data);
+});
+
+app.post('/api/items', (req, res) => {
+  const { title } = req.query;
+  if (title) {
+    data.push({ id: Date.now().toString(), title });
+    return res.json(data);
+  }
+
+  return res.status(400);
+})
+
+app.listen(3000, () => {
+  console.log('Server started');
+});
